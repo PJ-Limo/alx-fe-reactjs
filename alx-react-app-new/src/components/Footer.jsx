c28be5c28ac5d084d13742eaa19f7fb30bc36a9e
PJ-Limo <pjslense@gmail.com>
2024-09-04 00:08:35 +0300

    updated components in new app

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{backgroundColor:'navy', color: 'yellow', padding: '70px'}}>
+           <p style={{color: 'white'}}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{backgroundColor:'grey', color: 'navy', padding: '70px'}}>
+          <p style={{color: 'yellow', padding: '20px'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
