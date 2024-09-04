a94fc5a01455db95e52b844fea1c406dd978a6e2
PJ-Limo <pjslense@gmail.com>
2024-09-04 13:54:39 +0300

    updated recipe store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,28 +1,7 @@
-import create from "zustand";
+import create from 'zustand'
 
-const useRecipeStore = create((set) => ({
+const useRecipeStore = create(set => ({
   recipes: [],
-  favorites: [],
-
-  // Add recipe to favorites
-  addFavorite: (recipeId) =>
-    set((state) => ({ favorites: [...state.favorites, recipeId] })),
-
-  // Remove recipe from favorites
-  removeFavorite: (recipeId) =>
-    set((state) => ({
-      favorites: state.favorites.filter((id) => id !== recipeId),
-    })),
-
-  // Mock recommendation logic based on favorites
-  recommendations: [],
-  generateRecommendations: () =>
-    set((state) => {
-      const recommended = state.recipes.filter(
-        (recipe) => state.favorites.includes(recipe.id) && Math.random() > 0.5
-      );
-      return { recommendations: recommended };
-    }),
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
 }));
-
-export default useRecipeStore;
