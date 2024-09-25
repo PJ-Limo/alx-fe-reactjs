63936a5f917f082e2589c49204423069b737c186
PJ-Limo <pjslense@gmail.com>
2024-09-25 23:19:23 +0300

    added user search files

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import Search from './components/Search';
+
+const App = () => {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <Search />
+    </div>
+  );
+};
+
+export default App;
