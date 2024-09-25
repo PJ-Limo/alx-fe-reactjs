8e7b037f46cd1a7cf55c37480de108002dcbd06e
PJ-Limo <pjslense@gmail.com>
2024-09-25 23:51:44 +0300

    Created react app for github user search

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import Search from './components/Search';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="container m-4 text-gray-900 font-bold" >GitHub User Search</h1>
+      <Search />
+    </div>
+  );
+};
+
+export default App;
